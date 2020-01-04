f0c6eb7020a0357cde1da9672f2192e88884e42b
engramar <engramar.bollas@gmail.com>
2020-01-05 00:03:46 +1100

    Added NSW Crime Rate from Rob

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ blueprint_opaltrain = Blueprint('api_opaltrain', __name__, url_prefix='/api_opal
 blueprint_opaltraincardtype = Blueprint('api_opaltraincardtype', __name__, url_prefix='/api_opaltraincardtype')
 blueprint_contingentworkforce = Blueprint('api_contingentworkforce', __name__, url_prefix='/api_contingentworkforce')
 blueprint_acnc = Blueprint('api_acnc', __name__, url_prefix='/api_acnc')
+blueprint_crimerate = Blueprint('api_crimerate', __name__, url_prefix='/api_crimerate')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
@@ -609,6 +610,146 @@ class CharityABN(Resource):
             detail_dict['postcode'] = detail['postcode']
             detail_dict['staff___full_time'] = detail['staff___full_time']
             detail_dict['donations_and_bequests'] = detail['donations_and_bequests']
+            return_values.append(detail_dict)
+
+        return make_response(jsonify(return_values), 200)
+
+########################################################################################################################
+# NSW Crime Rate
+########################################################################################################################
+api = Api(blueprint_crimerate, doc='/documentation', version='1.0', title='Data Service for crime rate information by suburb',
+          description='This is a Flask-Restplus data service that allows a client to consume APIs to provide crime '
+                      'rate data for NSW suburbs from 1995 to 2018.',
+          contact='Robertus Van Den Braak', contact_email='robsphone@iinet.net.au',
+          default='Suburb Crime', default_label='Click here to list available end points to display suburb crime rates.'
+          )
+
+app.register_blueprint(blueprint_crimerate)
+
+#Database helper
+ROOT = path.dirname(path.realpath(__file__))
+
+def connect_db_crimerate():
+    sql = sqlite3.connect(path.join(ROOT, "SUBURB_CRIME_RATE.sqlite"))
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db_crimerate():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db_crimerate()
+    return g.sqlite_db
+
+@api.route('/suburb')
+class Suburb(Resource):
+    @api.response(200, 'SUCCESSFUL: Contents successfully loaded')
+    @api.response(204, 'NO CONTENT: No content in database')
+    @api.doc(description='Returns all suburbs available in the database.')
+    def get(self):
+        db = get_db_crimerate()
+        details_cur = db.execute('select SUBURB_ID, NAME from SUBURB')
+        details = details_cur.fetchall()
+
+        return_values = []
+
+        for detail in details:
+            detail_dict = {'suburbId': detail['SUBURB_ID'], 'name': detail['NAME']}
+            return_values.append(detail_dict)
+
+        return make_response(jsonify(return_values), 200)
+
+
+@api.route('/crimeRate/<string:SUBURB>', methods=['GET'])
+class CrimeRate(Resource):
+    @api.response(200, 'SUCCESSFUL: Contents successfully loaded')
+    @api.response(204, 'NO CONTENT: No content in database')
+    @api.doc(description='Retrieves all crime rate records for the specified suburb. Result set can be filtered '
+                         'by specifying start date, end date and crime category ID',
+             params={'startDate': 'YYYY-MM-DD', 'endDate': 'YYYY-MM-DD', 'crimeCategoryId': 'Integer referencing a '
+                                                                                            'Crime_Category_ID'})
+    def get(self, SUBURB):
+        startDate = request.args.get('startDate')
+        endDate = request.args.get('endDate')
+        categoryID = request.args.get('crimeCategoryId')
+        db = get_db_crimerate()
+        sql = "SELECT NAME, CRIME_RATE.CRIME_CATEGORY_ID, CRIME_CATEGORY.OFFENCE, CRIME_CATEGORY.SUBCATEGORY," \
+              " START_DATE, END_DATE, RATE " \
+              "FROM SUBURB " \
+              "JOIN CRIME_RATE ON SUBURB.SUBURB_ID = CRIME_RATE.SUBURB_ID " \
+              "JOIN CRIME_CATEGORY ON CRIME_CATEGORY.CRIME_CATEGORY_ID = CRIME_RATE.CRIME_CATEGORY_ID " \
+              "WHERE NAME = '%s' "
+        if startDate:
+            sql = sql + " AND START_DATE >= '" + startDate + "'"
+        if endDate:
+            sql = sql + " AND END_DATE <= '" + endDate + "'"
+        if categoryID:
+            sql = sql + " AND CRIME_RATE.CRIME_CATEGORY_ID = '" + categoryID + "'"
+        sql = sql + " ORDER BY CRIME_RATE.CRIME_CATEGORY_ID ASC, START_DATE ASC"
+        details_cur = db.execute(sql % SUBURB)
+        details = details_cur.fetchall()
+        return_values = []
+
+        for detail in details:
+            detail_dict = {'name': detail['NAME'], 'crimeCategoryId': detail['CRIME_CATEGORY_ID'],
+                           'offence': detail['OFFENCE'], 'subcategory': detail['SUBCATEGORY'],
+                           'startDate': detail['START_DATE'], 'endDate': detail['END_DATE'], 'rate': detail['RATE']}
+
+            return_values.append(detail_dict)
+
+        return make_response(jsonify(return_values), 200)
+
+
+@api.route('/crimeRateSummary/<string:SUBURB>', methods=['GET'])
+class CrimeRateSummary(Resource):
+    @api.response(200, 'SUCCESSFUL: Contents successfully loaded')
+    @api.response(204, 'NO CONTENT: No content in database')
+    @api.doc(description='Returns a summary total of all crime rates for the specified suburb from 1995 to 2018.'
+                         'A start date, end date parameter can be added to limit the summary',
+             params={'startDate': 'YYYY-MM-DD', 'endDate': 'YYYY-MM-DD'})
+    def get(self, SUBURB):
+        startDate = request.args.get('startDate')
+        endDate = request.args.get('endDate')
+        db = get_db_crimerate()
+        select = "SELECT NAME, CRIME_CATEGORY.OFFENCE, CRIME_CATEGORY.SUBCATEGORY, SUM(RATE) AS TOTAL_RATE " \
+                 "FROM SUBURB " \
+                 "JOIN CRIME_RATE ON SUBURB.SUBURB_ID = CRIME_RATE.SUBURB_ID " \
+                 "JOIN CRIME_CATEGORY ON CRIME_CATEGORY.CRIME_CATEGORY_ID = CRIME_RATE.CRIME_CATEGORY_ID " \
+                 "WHERE NAME = '%s' "
+        group = "GROUP BY NAME, CRIME_CATEGORY.OFFENCE, CRIME_CATEGORY.SUBCATEGORY"
+        if startDate:
+            select = select + " AND START_DATE >= '" + startDate + "'"
+        if endDate:
+            select = select + " AND END_DATE <= '" + endDate + "'"
+        sql = select + group
+        details_cur = db.execute(sql % SUBURB)
+
+        details = details_cur.fetchall()
+
+        return_values = []
+
+        for detail in details:
+            detail_dict = {'name': detail['NAME'], 'offence': detail['OFFENCE'], 'subcategory': detail['SUBCATEGORY'],
+                           'rate': detail['TOTAL_RATE']}
+            return_values.append(detail_dict)
+
+        return make_response(jsonify(return_values), 200)
+
+
+@api.route('/crimeCategory', methods=['GET'])
+class CrimeCategory(Resource):
+    @api.response(200, 'SUCCESSFUL: Contents successfully loaded')
+    @api.response(204, 'NO CONTENT: No content in database')
+    @api.doc(description='Returns all crime categories available in the database.')
+    def get(self):
+        db = get_db_crimerate()
+        details_cur = db.execute(
+            'select CRIME_CATEGORY_ID, OFFENCE, SUBCATEGORY from CRIME_CATEGORY')
+        details = details_cur.fetchall()
+
+        return_values = []
+
+        for detail in details:
+            detail_dict = {'crimeCategoryId': detail['CRIME_CATEGORY_ID'], 'offence': detail['OFFENCE'],
+                           'subcategory': detail['SUBCATEGORY']}
             return_values.append(detail_dict)
 
         return make_response(jsonify(return_values), 200)
