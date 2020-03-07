e96fae7d198505d63b0b4fe0ed7c16c739be5bfe
engramar <engramar.bollas@gmail.com>
2020-03-07 11:14:45 +1100

    NSW Goverment Schools

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ blueprint_contingentworkforce = Blueprint('api_contingentworkforce', __name__, u
 blueprint_acnc = Blueprint('api_acnc', __name__, url_prefix='/api_acnc')
 blueprint_crimerate = Blueprint('api_crimerate', __name__, url_prefix='/api_crimerate')
 blueprint_hospitals = Blueprint('api_hospitals', __name__, url_prefix='/api_hospitals')
+blueprint_schools = Blueprint('api_schools', __name__, url_prefix='/api_schools')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
@@ -877,6 +878,82 @@ class LHDAll(Resource):
         return_values = [detail['LocalHealthDistrict'] for detail in details]
 
         return make_response(jsonify(return_values), 200)        
+
+########################################################################################################################
+# NSW Government Schools
+########################################################################################################################
+api = Api(blueprint_schools, doc='/documentation', version='1.0', title='Data Service for NSW government schools information by suburb',
+          description='This is a Flask-Restplus data service that allows a client to consume APIs related to NSW government schools information by suburb.',
+          )
+app.register_blueprint(blueprint_schools)
+
+#Database helper
+ROOT = path.dirname(path.realpath(__file__))
+
+def connect_db_schools():
+    sql = sqlite3.connect(path.join(ROOT, "governmentschools.db"))
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db_schools():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db_schools()
+    return g.sqlite_db
+
+@api.route('/schools/all')
+class GovernmentSchoolsAll(Resource):
+    @api.response(200, 'SUCCESSFUL: Contents successfully loaded')
+    @api.response(204, 'NO CONTENT: No content in database')
+    @api.doc(description='Retrieving all records from the database for all suburbs.')
+    def get(self):
+        db = get_db_schools()
+        details_cur = db.execute('select * from [master_dataset]')
+        details = details_cur.fetchall()
+
+        return_values = []
+
+        for detail in details:
+            detail_dict = {}
+            detail_dict['School_name'] = detail['School_name']
+            detail_dict['Street'] = detail['Street']
+            detail_dict['Town_suburb'] = detail['Town_suburb']
+            detail_dict['Postcode'] = detail['Postcode']
+            detail_dict['Phone'] = detail['Phone']
+            detail_dict['School_email'] = detail['School_email']
+            detail_dict['Website'] = detail['Website']
+            detail_dict['Fax'] = detail['Fax']
+            return_values.append(detail_dict)
+
+        return make_response(jsonify(return_values), 200)
+
+@api.route('/schools/<string:SUBURB>', methods=['GET'])
+class GovernmentSchools(Resource):
+    @api.response(200, 'SUCCESSFUL: Contents successfully loaded')
+    @api.response(204, 'NO CONTENT: No content in database')
+    @api.doc(description='Retrieving all records from the database for one suburb.')
+    def get(self, SUBURB):
+        db = get_db_schools()
+        details_cur = db.execute(
+            'select * from [master_dataset] where Town_suburb = ? COLLATE NOCASE', [SUBURB])
+            # 'select * from [master_dataset] where Town_suburb = ? COLLATE NOCASE COLLATE RTRIM', [SUBURB])
+        details = details_cur.fetchall()
+
+        return_values = []
+
+        for detail in details:
+            detail_dict = {}
+            detail_dict['School_name'] = detail['School_name']
+            detail_dict['Street'] = detail['Street']
+            detail_dict['Town_suburb'] = detail['Town_suburb']
+            detail_dict['Postcode'] = detail['Postcode']
+            detail_dict['Phone'] = detail['Phone']
+            detail_dict['School_email'] = detail['School_email']
+            detail_dict['Website'] = detail['Website']
+            detail_dict['Fax'] = detail['Fax']
+            return_values.append(detail_dict)
+
+        return make_response(jsonify(return_values), 200)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
